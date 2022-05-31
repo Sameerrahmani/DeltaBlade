8b0970c08bc4e9ce699b42bc8c31d9cabe1ebaab
sameerrahmani <56892116+Sameerrahmani@users.noreply.github.com>
2022-05-30 20:45:52 -0400

    Create Global.cs

---
+++ Global.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public class Global : Node2D
+{
+	public static int Score;
+	public static int SwordDamage = 25;
+	public static int RotationSpeed = 2;
+	public static int PointsTillLvlUp = 45;
+	public static int Level = 1;
+}
